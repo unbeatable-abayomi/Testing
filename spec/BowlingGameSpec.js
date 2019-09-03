f3bb66e7fbf43211b13249daa435ea098b567c66
Abayomi igwubor <igwubor@outlook.com>
2019-09-03 13:50:52 +0100

    added_beforeEachFunction

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,15 +1,19 @@
-describe("Bowling game", function() {
-        
-    it("should properly calculate a gutter game", function() {
-        // test
-    });
-    
-    it("should properly calculate a strike", function () {
-        // test
-    });
-    
-    it("should properly calculate a spare", function () {
-        // test
-    });
-        
+describe('Bowling game', function() {
+	var game;
+
+	beforeEach(function() {
+		game = new BowlingGame();
+	});
+
+	it('should properly calculate a gutter game', function() {
+		// test
+	});
+
+	it('should properly calculate a strike', function() {
+		// test
+	});
+
+	it('should properly calculate a spare', function() {
+		// test
+	});
 });
