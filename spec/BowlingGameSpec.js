e9ba9d85df9463db6f267c3cc54c2d1d8f375cc0
Abayomi igwubor <igwubor@outlook.com>
2019-09-03 13:32:39 +0100

    firstCommit

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,15 @@
+describe("Bowling game", function() {
+        
+    it("should properly calculate a gutter game", function() {
+        // test
+    });
+    
+    it("should properly calculate a strike", function () {
+        // test
+    });
+    
+    it("should properly calculate a spare", function () {
+        // test
+    });
+        
+});
